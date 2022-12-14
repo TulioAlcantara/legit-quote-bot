d5a70fdf0e4ec6e0bf0fe8b18905f2f8c667d10f
tulio <tulioam9@gmail.com>
2022-12-14 11:49:41 -0300

    feat: add twitter integration

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
+const fs = require("fs");
 const axios = require("axios");
 const cheerio = require("cheerio");
+const { TwitterApi } = require("twitter-api-v2");
 require("dotenv").config();
 
 const QUOTE_URL = "https://api.quotable.io/random";
 const CHARACTER_URL =
   "https://bestrandoms.com/get-random-best-movie-characters-of-all-time";
 
-const generateQuote = async () => {
+const main = async () => {
   const quote = await getQuote();
-  const [character, picture] = await getCharacter();
+  const [character, pictureUrl] = await getCharacter();
+  await dowloadPicture(pictureUrl);
 
-  console.log(quote, character, picture);
-  //   console.log(process.env);
+  console.log(`${quote}\n${character}\n${pictureUrl}`);
+
+  const userClient = new TwitterApi({
+    appKey: process.env.TWITTER_API_KEY,
+    appSecret: process.env.TWITTER_API_SECRET,
+    accessToken: process.env.TWITTER_ACCESS_TOKEN,
+    accessSecret: process.env.TWITTER_ACCESS_SECRET,
+  });
+
+  // const mediaId = await userClient.v1.uploadMedia("./image.jpg");
+  // console.log(mediaId);
+
+  // await userClient.v2.tweet(`${quote}\n\n- ${character}`, {
+  //   media: {
+  //     media_ids: [mediaId],
+  //   },
+  // });
 };
 
 const getQuote = async () => {
@@ -25,17 +43,26 @@ const getCharacter = async () => {
   });
   const characterPageHtml = characterPageRes.data;
   const $ = cheerio.load(characterPageHtml);
-  return [
-    $(
-      "li.res-item:nth-child(1) > div:nth-child(1) > div:nth-child(2) > h4:nth-child(1)"
-    )
-      .text()
-      .split(" ")[1]
-      .trim(),
-    $(
-      "li.res-item:nth-child(1) > div:nth-child(1) > div:nth-child(1) > a:nth-child(1) > img:nth-child(1)"
-    ).attr("src"),
-  ];
+
+  let character = $(
+    "li.res-item:nth-child(1) > div:nth-child(1) > div:nth-child(2) > h4:nth-child(1)"
+  ).text();
+
+  const [id, ...actualName] = character.split(" ");
+  character = actualName.join(" ");
+
+  const pictureUrl = $(
+    "li.res-item:nth-child(1) > div:nth-child(1) > div:nth-child(1) > a:nth-child(1) > img:nth-child(1)"
+  ).attr("src");
+
+  return [character, pictureUrl];
 };
 
-generateQuote();
+const dowloadPicture = async (url) => {
+  const pictureRes = await axios.get(url, {
+    responseType: "stream",
+  });
+  pictureRes.data.pipe(fs.createWriteStream("image.jpg"));
+};
+
+main();
